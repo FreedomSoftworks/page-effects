2be5deb6ba6d3f7d494ff1bf72796d905fab6e89
Matt Mannion <mattpmannion@gmail.com>
2023-09-02 00:34:16 -0400

    header started

--- src/components/global/header/Header.vue
+++ src/components/global/header/Header.vue
@@ -1,11 +1,39 @@
 <template>
-  <header class="main">header</header>
+  <header>
+    <div class="main">
+      <div class="left">header</div>
+      <div class="right">
+        <div>one</div>
+        <div>two</div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
+header {
+  background-color: #222;
+}
+
 .main {
   @include PageWidth;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1rem 10rem;
+}
+
+.left {
+}
+
+.right {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2rem;
 }
 </style>
