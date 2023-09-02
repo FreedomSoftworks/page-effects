270ecaa073fb73fb272cad518e264096e75686ae
Matt Mannion <mattpmannion@gmail.com>
2023-09-01 23:56:14 -0400

    added base scss styles

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import '/src/assets/main.css';
-import '/src/scss/main.scss';
+import '/src/scss/_main/main.scss';
 
 import { createApp } from 'vue';
 import App from '/src/App.vue';
-import { router } from '/src/router';
+import { router } from './router/main.router';
 import { key, store } from '/src/store/main.store';
 import { StoreMutKeysPlugin } from '/src/store/plugins.store';
 import { SystemUtil } from '/src/util/system.util';
